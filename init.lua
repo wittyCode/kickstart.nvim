066949ed55da07a4b1ccae51f9011f55e6484a9e
Michael Wittmann <michael.wittmann@climatepartner.com>
2024-03-12 11:11:21 +0100

    feat: add neotree plugin for file browsing

--- init.lua
+++ init.lua
@@ -419,6 +419,17 @@ require('lazy').setup({
   },
 
   {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
+
+  {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
