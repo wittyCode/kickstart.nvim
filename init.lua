0f03a19b710d0086a9ff693c28db0f9f8ef7ea18
Michael Wittmann <michael.wittmann@climatepartner.com>
2024-03-13 11:55:13 +0100

    feat: add cool keymaps; add vim game plugin; add vim opts for tab size

--- init.lua
+++ init.lua
@@ -118,6 +118,14 @@ vim.opt.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.opt.breakindent = true
 
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.incsearch = true
+
 -- Save undo history
 vim.opt.undofile = true
 
@@ -156,12 +164,18 @@ vim.opt.scrolloff = 10
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- remap to not replace buffer with token that was pasted over
+vim.keymap.set('x', '<leader>p', '"_dP')
+vim.keymap.set('n', '<leader>E', ':Neotree<CR>')
+vim.keymap.set('n', '<leader>EG', ':Neotree git_status float<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Show [D]iagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -870,6 +884,9 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+
+  -- game-ified VIM learning
+  { 'ThePrimeagen/vim-be-good' },
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
