a109e8e8eeff7768ec3ea89a8356be8b4492b44d
wittycode <michael.wittmann2018@gmail.com>
2024-07-26 16:19:24 +0200

    feat: add completion to dap repl

--- lua/kickstart/plugins/autocomplete.lua
+++ lua/kickstart/plugins/autocomplete.lua
@@ -23,6 +23,7 @@ return { -- Autocompletion
     -- Adds other completion capabilities.
     --  nvim-cmp does not ship with all sources by default. They are split
     --  into multiple repos for maintenance purposes.
+    'rcarriga/cmp-dap',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-vsnip',
@@ -58,6 +59,10 @@ return { -- Autocompletion
         end,
       },
       completion = { completeopt = 'menu,menuone,noinsert' },
+
+      enabled = function()
+        return vim.api.nvim_buf_get_option(0, 'buftype') ~= 'prompt' or require('cmp_dap').is_dap_buffer()
+      end,
 
       formatting = {
         format = function(entry, vim_item)
@@ -118,5 +123,10 @@ return { -- Autocompletion
         { name = 'path' },
       },
     }
+    cmp.setup.filetype({ 'dap-repl', 'dapui_watches', 'dapui_hover' }, {
+      sources = {
+        { name = 'dap' },
+      },
+    })
   end,
 }
